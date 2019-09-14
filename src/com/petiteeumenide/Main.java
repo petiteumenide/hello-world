ec7e446b14092a74c77e2615664d55cef0e6407f
petiteeumenide <dominique.batifol@gmail.com>
2019-09-14 13:08:34 +0200

    Mon premier commit

---
+++ src/com/petiteeumenide/Main.java
@@ -0,0 +1,9 @@
+package com.petiteeumenide;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Je suis bien cool");
+    }
+}
